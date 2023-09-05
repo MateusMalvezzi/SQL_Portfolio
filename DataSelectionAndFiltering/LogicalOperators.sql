051a548d9fd300c17d3daa1943e0f0d2c9010f71
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-09-04 23:15:35 -0300

    Logical Operators

---
+++ DataSelectionAndFiltering/LogicalOperators.sql
@@ -0,0 +1,19 @@
+
+
+USE ContosoRetailDW
+
+SELECT
+	EmailAddress
+	,Gender
+	,MaritalStatus
+	,TotalChildren
+	,NumberChildrenAtHome
+	,NumberCarsOwned AS'Quantidade de carros'
+	,Education
+FROM DimCustomer
+WHERE 
+	Education = 'High School' 
+	AND NumberChildrenAtHome >= 4 
+	AND NOT MaritalStatus = 'S'
+	
+ORDER BY 2 DESC, 3 DESC, 4 DESC, [Quantidade de carros] DESC
