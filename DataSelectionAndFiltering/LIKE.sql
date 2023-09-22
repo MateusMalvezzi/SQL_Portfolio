9fb3600376a9459f663600a71f9b397bae4f4c08
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-09-21 22:31:43 -0300

    LIKE

---
+++ DataSelectionAndFiltering/LIKE.sql
@@ -0,0 +1,55 @@
+
+
+USE ContosoRetailDW
+
+
+--Buscando nome completo
+SELECT
+	FirstName + ' ' + LastName AS FullName
+	,*
+FROM DimEmployee
+WHERE FirstName + ' ' + LastName LIKE 'Aaron Painter'
+
+
+
+-- Buscando quantidade de aparições com o mesmo nome
+SELECT
+	FirstName + ' ' + LastName AS FullName
+	,*
+FROM DimEmployee
+WHERE FirstName + ' ' + LastName LIKE 'Aaron%'
+
+
+
+-- Contem um pedaço do nome (nesse caso, david) no first, middle ou last
+SELECT
+	FirstName + ' ' + LastName AS FullName
+	,*
+FROM DimEmployee
+WHERE 
+	FirstName + ' ' + LastName LIKE '%david%'
+	AND MaritalStatus LIKE 'M'
+
+
+-- Termina com algum nome, nesse caso, 'Abercrombie')
+SELECT
+	FirstName + ' ' + LastName AS FullName
+	,*
+FROM DimEmployee
+WHERE FirstName + ' ' + LastName LIKE '%Abercrombie'
+
+
+-- Começa com algum nome, nesse caso, 'Michael')
+SELECT
+	FirstName + ' ' + LastName AS FullName
+	,*
+FROM DimEmployee
+WHERE FirstName + ' ' + LastName LIKE 'Michael%'
+
+
+-- Começa de uma forma e termina de outro, nesse caso, 'Da___')
+SELECT
+	FirstName + ' ' + LastName AS FullName
+	,*
+FROM DimEmployee
+WHERE FirstName + ' ' + LastName LIKE 'Da___%'
