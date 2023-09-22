9ebe92155c77ef3f7d987186f9e8ac45e9c193da
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-09-21 21:41:42 -0300

    adding more NOT in my query

--- DataSelectionAndFiltering/NOT.sql
+++ DataSelectionAndFiltering/NOT.sql
@@ -16,4 +16,5 @@ WHERE
 	MaritalStatus IS NOT NULL
 	AND TotalChildren BETWEEN 2 and 5
 	AND NOT BirthDate BETWEEN '1930-01-01' AND '1960-01-01'
+	AND NumberChildrenAtHome NOT IN (0, 5, 4)
 ORDER BY TotalChildren DESC
