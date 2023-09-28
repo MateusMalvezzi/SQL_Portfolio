e235435ac80bd9b6f92bfe54f531a037e7898f6c
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-09-27 22:14:17 -0300

    Full Join

---
+++ JoinTypes/FullJoin.sql
@@ -0,0 +1,11 @@
+
+
+USE ContosoRetailDW
+-- Todos da direita e todos da esquerda
+SELECT DISTINCT
+	 P.ProductKey
+	,P.ProductName
+	,S.ProductKey AS 'S.ProductKey'
+FROM DimProduct AS P
+FULL JOIN FactSales AS S ON S.ProductKey = P.ProductKey
+ORDER BY 3
