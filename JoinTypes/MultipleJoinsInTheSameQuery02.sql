58f871941db841c92f220b1757167fec2d4f6278
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-09-30 22:41:41 -0300

    MultipleJoinsInTheSameQuery02

---
+++ JoinTypes/MultipleJoinsInTheSameQuery02.sql
@@ -0,0 +1,11 @@
+
+USE ContosoRetailDW
+
+SELECT TOP 100
+	S.SalesKey
+	,S.SalesAmount
+	,ST.StoreName
+	,G.ContinentName
+FROM FactSales AS S
+INNER JOIN DimStore AS ST ON ST.StoreKey = S.StoreKey
+INNER JOIN DimGeography AS G ON G.GeographyKey = ST.GeographyKey
