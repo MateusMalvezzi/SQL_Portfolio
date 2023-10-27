84f41001a8051ecd62b0db6d8b05c74320966463
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2023-10-27 18:24:23 -0300

    PARTITION BY

---
+++ WindowFunction/PartitionBy.sql
@@ -0,0 +1,14 @@
+
+
+
+SELECT
+	   [TrackId]
+      ,[Name]
+      ,[Composer]
+      ,[Milliseconds]
+      ,[Bytes]
+      ,[UnitPrice]
+	  ,ROW_NUMBER() OVER(ORDER BY Milliseconds ASC) AS 'ROW_NUMBER'
+	  ,ROW_NUMBER() OVER(PARTITION BY Composer ORDER BY Milliseconds ASC) AS 'ROW_NUMBER_PARTITION_BY'
+FROM [Chinook].[dbo].[Track]
+WHERE Composer IS NOT NULL
